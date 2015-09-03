6d767c5962e03a7005f31b8d17197ad9c017f0d1
calebgregory <calebgregory@gmail.com>
2015-09-03 12:20:38 -0500

    Adding postgres to findAll, create, bootstrap

--- app/post/Post.js
+++ app/post/Post.js
@@ -5,6 +5,50 @@ var _ = require('lodash');
 
 var mongo = require('../../lib/mongo/');
 
+var pg = require('pg');
+var url = 'postgres://localhost:5432'
+
+var query = (sql, params, cb) => {
+  var callback = typeof params === 'function' ? params : cb;
+
+  pg.connect(`${url}/atmebro`, (err, db, done) => {
+    if(err) throw err;
+
+    if(typeof params === 'function') {
+      db.query(sql, (err, res) => {
+        if(err) throw err;
+        callback(err, res.rows);
+      });
+    } else {
+      db.query(sql, params, (err, res) => {
+        if(err) throw err;
+        callback(err, res.rows);
+      });
+    }
+  })
+};
+
+(function bootstrap() {
+  pg.connect(url, (err, db, done) => {
+    if(err) throw err;
+
+    db.query('CREATE DATABASE atmebro;', err => {
+      if(err.message === 'database "atmebro" already exists') {
+        pg.connect(`${url}/atmebro`, (err, db, done) => {
+          db.query('CREATE TABLE IF NOT EXISTS posts ('+
+            '_id SERIAL PRIMARY KEY NOT NULL,'+
+            'text VARCHAR(40) NOT NULL'+
+            ');', (err,res) => {
+              done();
+            });
+        });
+      } else if(err) {
+        throw err;
+      }
+    });
+  });
+})();
+
 function Post(p) {
   this.text = p.text;
 }
@@ -16,11 +60,13 @@ Object.defineProperty(Post, 'collection', {
 });
 
 Post.count = function (cb) {
-  return Post.collection.count(cb);
+  query('SELECT COUNT(*) FROM posts;', cb);
+//  return Post.collection.count(cb);
 };
 
 Post.create = function (post, cb) {
-  Post.collection.insertOne(post, cb);
+  query(`INSERT INTO posts (text) VALUES ($1);`, [post.text], cb);
+//  Post.collection.insertOne(post, cb);
 };
 
 Post.setHidden = function (id, cb) {
@@ -40,14 +86,22 @@ Post.findById = function (id, cb) {
   });
 };
 
+
 Post.findAll = function (cb) {
-  Post.collection.find({hidden: {$ne: true}}).toArray(function (err, posts) {
+  query('SELECT * FROM posts;', (err,posts) => {
     var prototypedPosts = posts.map(function (post) {
       return setPrototype(post);
     });
 
     cb(err, prototypedPosts);
   });
+//  Post.collection.find({hidden: {$ne: true}}).toArray(function (err, posts) {
+//    var prototypedPosts = posts.map(function (post) {
+//      return setPrototype(post);
+//    });
+//
+//    cb(err, prototypedPosts);
+//  });
 };
 
 module.exports = Post;
